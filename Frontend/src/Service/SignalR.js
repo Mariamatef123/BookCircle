bdfa58350c14f65490022971b68e60ce221ab754
Mariamatef123 <154092598+Mariamatef123@users.noreply.github.com>
2026-07-19 20:16:01 +0300

    replace localhost

--- Frontend/src/Service/SignalR.js
+++ Frontend/src/Service/SignalR.js
@@ -7,6 +7,7 @@ const HUB_URL = `${import.meta.env.VITE_API_URL}/hubs/notifications`;
 class SignalRService {
   connection = null;
   startPromise = null;
+  
   callbacks = new Set();
   currentUserId = null; 
 
